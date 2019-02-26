5bc75ffdd711282f0f5432faa1f0e13f32d2f278
XiaomuSoftware <44538191+XiaomuSoftware@users.noreply.github.com>
2019-02-26 22:28:55 +0800

    Add files via upload

---
+++ 源码完整文件夹/小米主题下载器/小米主题下载器/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace 小米主题下载器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dz1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dz2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dz1
+            // 
+            this.dz1.Font = new System.Drawing.Font("楷体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dz1.Location = new System.Drawing.Point(98, 7);
+            this.dz1.Name = "dz1";
+            this.dz1.Size = new System.Drawing.Size(485, 32);
+            this.dz1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "主题链接:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "下载地址:";
+            // 
+            // dz2
+            // 
+            this.dz2.Font = new System.Drawing.Font("楷体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dz2.Location = new System.Drawing.Point(98, 46);
+            this.dz2.Name = "dz2";
+            this.dz2.Size = new System.Drawing.Size(485, 32);
+            this.dz2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(15, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "地址解析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(168, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "直接下载";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(324, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "主题商店";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(480, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 35);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "反馈 @小木";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 137);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dz2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dz1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "小米主题下载器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dz1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dz2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 源码完整文件夹/小米主题下载器/小米主题下载器/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 小米主题下载器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            //默认填写的主题链接
+            this.dz1.Text = "http://zhuti.xiaomi.com/detail/18528ddf-90b0-40cb-b538-71c3768d4f29";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //调用系统默认的浏览器 
+            System.Diagnostics.Process.Start("http://wpa.qq.com/msgrd?v=3&uin=451508532&site=qq&menu=yes");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://zhuti.xiaomi.com/detail/18528ddf-90b0-40cb-b538-71c3768d4f29");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //获取主题链接
+            string dz1 = this.dz1.Text;
+            //定义新旧字符串
+            string oldtext = "http://zhuti.xiaomi.com/detail/";
+            string newtext = "http://thm.market.xiaomi.com/thm/download/v2/";
+            //修改主题链接
+            string dz = string.Empty;
+            dz = dz1.Replace(oldtext,newtext);
+            //指定请求
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(dz);
+            //得到返回
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //得到流
+            Stream recStream = response.GetResponseStream();
+            //编码方式
+            Encoding gb2312 = Encoding.GetEncoding("gb2312");
+            //指定转换为gb2312编码
+            StreamReader sr = new StreamReader(recStream, gb2312);
+            //以字符串方式得到网页内容
+            String content = sr.ReadToEnd();
+            //将网页内容进行过滤
+            JObject obj = Newtonsoft.Json.Linq.JObject.Parse(content);
+            //判断是否获取到正确下载地址
+            if (obj["apiCode"].ToString() == "0")
+            {
+                string dz2 = obj["apiData"]["downloadUrl"].ToString();
+                this.dz2.Text = dz2;
+            }
+            else
+            {
+                this.dz2.Text = "请检查网络是否连接或者主题链接是否正确";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+            //判断地址解析是否正确
+            if (this.dz2.Text == "请检查网络是否连接或者主题链接是否正确")
+            {
+                MessageBox.Show("无法解析到正确的地址,不能直接下载,请检查网络或者输入是否有误");
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(this.dz2.Text);
+            }
+        }
+    }
+}
